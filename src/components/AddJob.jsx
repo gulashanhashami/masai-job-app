89434440e8192e7511db9d4c831a04fe8d53db22
Gulashan Hashami <gulashan959898@gmail.com>
2022-12-03 12:33:21 +0530

    added files

---
+++ src/components/AddJob.jsx
@@ -0,0 +1,120 @@
+import axios from "axios";
+import { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+const Stylediv=styled.div`
+font-family:   Arial, sans-serif;
+ .title{
+     margin: auto;
+     width: 45%;
+     font-size:1.2vw;
+     color: red;
+    //   border: 1px solid red; 
+ }
+  form{
+      width: 45%;
+      height: auto;
+      /* border: 1px solid red; */
+      margin: auto;
+  } 
+  input{
+      width: 60%;
+      height: 3.5vh;
+      font-size:1vw;
+  }
+  p{
+      font-size:1.2vw;
+      line-height:.1vh;
+  }
+
+  .select{
+    font-size:1.2vw;
+  }
+  .option{
+      width: 45%;
+      height: 4.3vh;
+      font-size:1.2vw;
+  }
+  #btn{
+      width: 30%;
+      height: 4.5vh;
+      font-weight: bold;
+      font-size:1.2vw;
+      background-color: green;
+      color: white;
+      border: 2px solid green;
+      border-radius: 3px;
+      margin-left: 14%;
+  }
+  #btn:hover{
+      background-color: white;
+      color: red;
+  }
+`;
+export const AddJob=()=>{
+const [pdata, setData] =useState({})
+
+const handleChange=(e)=>{
+   let key=e.target.name;
+       setData(
+           {
+               ...pdata,
+               [key]: e.target.value,
+           }
+       )
+}
+
+   return (
+      
+           <Stylediv>
+       <div className="title">
+       <h1>Add the company</h1>
+       </div>
+       <form onSubmit={(e)=>{
+          
+           e.preventDefault();
+           axios.post(`http://localhost:3001/jobData`, pdata).then((res)=>{
+              setData({
+                company_name:"",
+                company_address:"",
+                  city:"",
+                  state_name:"",
+                  start_date:"",
+                  position:"",
+                  contract:"",
+                  description:"",
+              })
+           })
+       }}>
+       <p>Company name</p>
+       <input type="text" name="company_name" value={pdata.company_name} onChange={handleChange} placeholder="Company name" required />
+        <p>Enter city</p>
+       <input type="text" name="city" value={pdata.city} onChange={handleChange} placeholder="City" required />
+       <p>Enter Location</p>
+       <input type="text" name="company_address" value={pdata.company_address} onChange={handleChange} placeholder="Location" required />
+       <p>Role</p>
+       <input type="text" name="role_name" value={pdata.role_name} onChange={handleChange} placeholder="role" required />
+       <p>Level</p>
+       <input type="text" name="level_date" value={pdata.level_date} onChange={handleChange} required />
+       <p>Position</p>
+       <input type="text" name="position" value={pdata.position} onChange={handleChange} required />
+       <p>Language</p>
+       <input type="text" name="language" value={pdata.language} onChange={handleChange} placeholder="language" required />
+       <br />
+       <br />
+     <span className="select">Cotract
+       <select name="contract" value={pdata.contract} onChange={handleChange} className="option" required>
+           <option value="0">Select</option>
+           <option value="full">Full Time</option>
+           <option value="part">Part Time</option>
+       </select></span>
+       <p>Date</p>
+       <input type="date" name="postedAt" value={pdata.postedAt} onChange={handleChange} required />
+       <br />
+       <br />
+      <input id="btn" type="submit"  value="Submit" />
+       </form>
+</Stylediv>
+    
+   )
+}
---
+++ src/components/JobListingPage.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import axios from "axios";
+import { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { useEffect } from 'react';
+
+const Stylediv=styled.div`
+.itembox{
+   width: 95%;
+   height: 6vh;
+
+   border: 1px solid gray;
+   margin: auto;
+}
+  .logoCompany{
+    width: 90%;
+    height: 5vh;
+    display: flex;
+    flex-direction: row;
+   align-items: center;
+   gap: 10px;
+   margin: auto;
+  }
+  .image{
+    width: 7%;
+    height: 90%;
+  }
+`;
+export const JobListingPage=()=> {
+const [jobData, setJobData]= useState([])
+    useEffect(()=>{
+        getJobData();
+    },[])
+
+    const getJobData=()=>{
+        axios.get("http://localhost:3001/jobData").then(({data})=>{
+            console.log(data)
+            setJobData(data)
+    });
+    }
+
+  return (
+    <>
+    <Stylediv>
+    <div>JobListingPage</div>
+    <div>
+        {jobData.map((item)=>{
+            return (
+                <div className='itembox' key={item.id}>
+                   <div className='logoCompany'>
+                    <img className='image' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0HT0GRHVKL3StouKXuJl31O5z-xf-HjtfiQ&usqp=CAU" alt="" />
+                      <div>
+                        <p>{item.company_name}</p>
+                        <p>{item.position}</p>
+                      </div>
+                   </div>
+                </div>
+            )
+        })}
+    </div>
+    </Stylediv>
+    </>
+  )
+}
+
+export default JobListingPage
